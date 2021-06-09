edca7f748fbd513f6734f0f5dd58ec1a82844ce7
jake <jpollard@ucsd.edu>
2021-06-09 11:57:48 -0700

    more

--- leakageFuncs.py
+++ leakageFuncs.py
@@ -1,43 +1,45 @@
-from tensorflow import keras
-import tensorflow as tf
-from keras import layers
-from keras import backend as k
-from keras.utils.np_utils import to_categorical
 import numpy as np
-from funcs import getTestError,getGrad
-def getModel(batchSize=1):
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch import autograd
+class Network(nn.Module):
     """
-    Returns a simple CNN model
+    Simple CNN model
     """
-    model = keras.Sequential()
-    model.add(layers.Conv2D(64,kernel_size=3,activation='sigmoid'))
-    model.add(layers.MaxPooling2D((2,2),(1,1),padding='valid'))
-    model.add(layers.Conv2D(32,kernel_size=3,activation='sigmoid'))
-    model.add(layers.MaxPooling2D((2,2),(1,1),padding='valid'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(10,activation='softmax'))
-    model.compile(optimizer='SGD',loss='categorical_crossentropy',metrics=['accuracy'])
-    #instantiate with minibatchsize=
-    t = model(np.zeros((batchSize,28,28,1)))
-    #model.summary()
-    return model
-def getTrueGradient(model,features,labels,testFeatures,testLabels,session,stopIter=10000,learningRate=1e-2,batchSize=1):
-    gen = np.random.default_rng()
-    lossF = keras.losses.CategoricalCrossentropy(from_logits=False)
-    for i in range(stopIter):
-        idx = gen.choice(np.arange(len(labels)),batchSize)
-        batchFeatures = features[idx]
-        batchLabels = labels[idx]
+    def __init__(self):
+        super(Network,self).__init__()
+        self.layers = nn.Sequential(
+        nn.Conv2d(1,32,kernel_size=5,stride=1,padding=2),nn.Sigmoid(),
+        nn.MaxPool2d(2,1,padding=1),
+        nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1),nn.Sigmoid(),
+        nn.MaxPool2d(2,1,padding=1))
+        self.fc = nn.Sequential(nn.Linear(57600,10))
+    def forward(self,x):
+        out = self.layers(x)
+        out = out.view(out.size(0),-1)
+        out = self.fc(out)
+        return out
 
-        #calc grad
-        numericGrad = getGrad(model,lossF,batchFeatures,batchLabels)
-        #set weights manually
-        curr = np.array(model.get_weights(),dtype='object')
-        grad = np.array(numericGrad,dtype='object')
-        model.set_weights(curr-learningRate*grad)
-        #print iter
-        if i%10==0:
-            print("iter {}".format(i))
-        print('test error: {}'.format(getTestError(model,testFeatures,testLabels,batchSize=1)))
-        return (grad,curr)
-    
+
+def trainAndReleaseGrads(model,trainData,stopIter,crit,opt):
+    """
+    Trains the model for a small number of iterations, then returns
+    the gradient and model
+    """
+    model.train()
+    for idx, (features,labels) in trainData:
+        opt.zero_grad()
+        out = model(features)
+        loss = crit(out,labels)
+        #if we are done, release gradient
+        if idx >stopIter:
+            gradient = autograd.grad(loss,model.parameters())
+            trueGrad = list((_.detach().clone() for _ in gradient))
+            return model,trueGrad
+        lossVal = loss.item()
+        loss.backward()
+        opt.step()
+        if idx%1000==0:
+            print("Example: {}, loss: {}".format(idx,lossVal))
+
