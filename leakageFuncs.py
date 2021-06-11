a5ac7f7f95119e82671ea9ca0edbf8315e3f16de
jake <jpollard@ucsd.edu>
2021-06-10 18:54:27 -0700

    notebooks

--- leakageFuncs.py
+++ leakageFuncs.py
@@ -3,6 +3,10 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from torch import autograd
+import torchvision
+from torchvision import transforms
+toImage = transforms.ToPILImage()
+from PIL import Image
 class Network(nn.Module):
     """
     Simple CNN model
@@ -22,13 +26,13 @@ class Network(nn.Module):
         return out
 
 
-def trainAndReleaseGrads(model,trainData,stopIter,crit,opt):
+def trainAndReleaseGrads(model,trainData,stopIter,lossCutoff,crit,opt):
     """
     Trains the model for a small number of iterations, then returns
     the gradient and model
     """
     model.train()
-    for idx, (features,y) in trainData:
+    for idx, (features,y) in enumerate(trainData):
         opt.zero_grad()
         out = model(features)
         #turn to one hot
@@ -36,29 +40,51 @@ def trainAndReleaseGrads(model,trainData,stopIter,crit,opt):
         labels[y[0]] = 1
         loss = crit(out,labels)
         #if we are done, release gradient
-        if idx > stopIter:
+        if idx >= stopIter and loss.item() <= lossCutoff:
             gradient = autograd.grad(loss,model.parameters())
             trueGrad = list((_.detach().clone() for _ in gradient))
-            return model,trueGrad
+            finLoss = loss.item()
+            return finLoss,model,trueGrad
         lossVal = loss.item()
         loss.backward()
         opt.step()
-        if idx%1000==0:
-            print("Example: {}, loss: {}".format(idx,lossVal))
-
-#https://github.com/mit-han-lab/dlg
-def closure():
-    optimizer.zero_grad()
-    pred = model(x)
-    oneHotY = F.softmax(y,dim=-1)
-    modelLoss = crit(pred,oneHotY)
-    leakGrad = autograd.grad(modelLoss,model.parameters(),create_graph=True)   
-    distance = 0
-    for gx,gy in zip(leakGrad,grad):
-        distance += ((gx - gy)**2).sum()
-    distance.backward()
-    return distance
 
 #must use this as pytorch does not support one hot for cat crosentropy
 def cross_entropy_for_onehot(pred, target):
-    return torch.mean(torch.sum(- target * F.log_softmax(pred, dim=-1), 1))
+        return torch.mean(torch.sum(- target * F.log_softmax(pred, dim=-1), 1))
+
+def runTrial(model,trainData,stopIter,lossCutoff,crit,opt,nextIter):
+    finLoss,model,grad = trainAndReleaseGrads(model,trainData,stopIter,lossCutoff,crit,opt)
+    x = torch.randn(1,1,28,28).requires_grad_(True)
+    y = torch.randn(1,10).requires_grad_(True)
+    res = []
+    optimizer = torch.optim.LBFGS([x,y])
+    for i in range(nextIter):
+        #https://github.com/mit-han-lab/dlg
+        def closure():
+            optimizer.zero_grad()
+            pred = model(x)
+            oneHotY = F.softmax(y,dim=-1)
+            modelLoss = crit(pred,oneHotY)
+            leakGrad = autograd.grad(modelLoss,model.parameters(),create_graph=True)   
+            distance = 0
+            for gx,gy in zip(leakGrad,grad):
+                distance += ((gx - gy)**2).sum()
+            distance.backward()
+            return distance
+        optimizer.step(closure)
+        cur = closure()
+        res.append((cur.item(),x))
+    return {"originalLoss":finLoss, "leakResults":res}
+
+
+def normalizeImage(img):
+    mi = min(img.flatten()).item()
+    img = torch.add(img,-1*mi)
+    ma = max(img.flatten()).item()
+    img = img/ma
+    return img
+
+def displayImage(reg):
+    im = toImage(torch.reshape(reg,(28,28)))
+    display(im.resize((5 * 28, 5 * 28), Image.NEAREST))
